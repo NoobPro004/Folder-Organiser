ac999cf058c2aa56d204ef48743b9a4b9fd531bd
Him_garg <himanshugarg738@gmail.com>
2021-05-26 00:31:29 +0530

    Completed Organize File

---
+++ commands/help.js
@@ -0,0 +1,14 @@
+let fs=require("fs");
+let path=require("path");
+function helpFn(dirPath){
+    console.log(`
+    List of All commands:
+                    node main.js tree "directoryPath"
+                    node main.js organize "directoryPath"
+                    node main.js help
+    `)
+    }
+
+    module.exports={
+        helpKey:helpFn
+    }
